b2b9474627f3afcc04df6c5caf8709ef9d6cfa5a
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-05 03:43:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,24 +1,9 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  collection.each do |item|
-    return item if item[:item] == name
-  end
+  collection.each { |item| return item if item[:item] == name }
   nil
 end
-
-    [
-      {:item => "AVOCADO", :price => 3.00, :clearance => true},
-      {:item => "KALE", :price => 3.00, :clearance => false},
-      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
-      {:item => "ALMONDS", :price => 9.00, :clearance => false},
-      {:item => "TEMPEH", :price => 3.00, :clearance => true},
-      {:item => "CHEESE", :price => 6.50, :clearance => false},
-      {:item => "BEER", :price => 13.00, :clearance => false},
-      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
-      {:item => "BEETS", :price => 2.50, :clearance => false},
-      {:item => "SOY MILK", :price => 4.50, :clearance => true}
-    ]
 
 def consolidate_cart(cart)
   consolidated_cart = []
@@ -36,6 +21,3 @@ def consolidate_cart(cart)
   end
   consolidated_cart
 end
-
-
-  
