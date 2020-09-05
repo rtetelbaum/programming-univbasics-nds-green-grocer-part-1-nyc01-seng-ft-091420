975f8062aafabdbf4edb494e4addc7acba86e32a
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-05 02:46:44 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -23,11 +23,13 @@ end
 def consolidate_cart(cart)
   consolidated_cart = []
   cart.each do |item|
-    if !find_item_by_name_in_collection(item, cart)
+    if !find_item_by_name_in_collection(item, consolidated_cart)
       consolidated_cart << item
       consolidated_cart.each do |consolidated_item|
         consolidated_item[:count] = 1 if consolidated_item[:item] == item[:item]
       end
+      binding.pry
+    elsif find_item_by_name_in_collection(item, cart)
       
     end
   end
