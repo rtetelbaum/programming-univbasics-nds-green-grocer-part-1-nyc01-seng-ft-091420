e41ace13d3f69d48d1c44ce472df2f79c3e3f1f6
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-05 02:17:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,6 +7,19 @@ def find_item_by_name_in_collection(name, collection)
   nil
 end
 
+    [
+      {:item => "AVOCADO", :price => 3.00, :clearance => true},
+      {:item => "KALE", :price => 3.00, :clearance => false},
+      {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+      {:item => "ALMONDS", :price => 9.00, :clearance => false},
+      {:item => "TEMPEH", :price => 3.00, :clearance => true},
+      {:item => "CHEESE", :price => 6.50, :clearance => false},
+      {:item => "BEER", :price => 13.00, :clearance => false},
+      {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+      {:item => "BEETS", :price => 2.50, :clearance => false},
+      {:item => "SOY MILK", :price => 4.50, :clearance => true}
+    ]
+
 def consolidate_cart(cart)
   consolidated_cart = []
   cart.each do |hashes|
