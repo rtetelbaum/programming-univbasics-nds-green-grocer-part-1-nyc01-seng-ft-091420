fc33f60b09a4f94f06fbe505dc515c74cf056f99
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-05 02:12:18 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |item|
-    return item if item[:item] == name
+    return item if item[:item] == name else nil
   end
 end
 
